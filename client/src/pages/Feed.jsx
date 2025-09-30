38c429813c99d990d461857609a3a7e89e69a8d2
Valeria Mikhalevich <valeerriam@gmail.com>
2025-09-30 22:54:43 +0300

    add stories

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
+import StoriesBar from "../components/StoriesBar";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [feeds, setfeeds] = useState([]);
+  const [loading, setLoading] = useState([true]);
+
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+      {/* Stories and post list */}
+      <div>
+        <StoriesBar />
+        <div className="p-4 space-y-6">Посты</div>
+      </div>
+
+      {/* Right sidebar */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Недавние сообщения</h1>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default Feed;
