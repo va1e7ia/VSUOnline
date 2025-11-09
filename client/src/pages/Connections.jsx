f1f74be876a6e061c601eb376d86462ef8fd34f6
Valeria Mikhalevich <valeerriam@gmail.com>
2025-11-09 20:30:18 +0300

    feed page, messages, start connections

--- client/src/pages/Connections.jsx
+++ client/src/pages/Connections.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Users,
+  UserPlus,
+  UserCheck,
+  UserRoundPen,
+  MessageSquare,
+} from "lucide-react";
+import { data, useNavigate } from "react-router-dom";
+import {
+  dummyConnectionsData as connections,
+  dummyFollowersData as followers,
+  dummyFollowingData as following,
+  dummyPendingConnectionsData as pendingConnections,
+} from "../assets/assets";
 
 const Connections = () => {
-  return <div>Connections</div>;
+  const [currentTab, setCurrentTab] = useState("Подписчики");
+
+  const navigate = useNavigate();
+  const dataArray = [
+    { label: "Подписчики", value: followers, icon: Users },
+    { label: "Подписки", value: following, icon: UserCheck },
+    { label: "Заявки", value: pendingConnections, icon: UserRoundPen },
+    { label: "Приняли заявку", value: connections, icon: UserPlus },
+  ];
+
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <div className="max-w-6xl mx-auto p-6">
+        {/*Title*/}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">Контакты</h1>
+          <p className="text-slate-600">
+            Управляй своей сетью и находи новых знакомых
+          </p>
+        </div>
+        {/*Counts*/}
+        <div className="mb-8 flex flex-wrap gap-6">
+          {dataArray.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center gap-1 border h-20 w-40 border-gray-200 bg-white shadow rounded-md"
+            >
+              <b>{item.value.length}</b>
+              <p className="text-slate-600">{item.label}</p>
+            </div>
+          ))}
+        </div>
+        {/*Tabs*/}
+        <div className="inline-flex flex-wrap items-center border border-gray-200 rounded-md p-1 bg-white shadow-sm">
+          {dataArray.map((tab) => (
+            <button
+              onClick={() => setCurrentTab(tab.label)}
+              key={tab.label}
+              className={`cursor-pointer flex items-center px-3 py-1 text-sm rounded-md transition-colors ${
+                currentTab === tab.label
+                  ? "bg-white font-medium text-black"
+                  : `text-gray-500 hover:text-black`
+              }`}
+            >
+              <tab.icon className="w-4 h-4" />
+              <span className="ml-1">{tab.label}</span>
+              {tab.count !== undefined && (
+                <span className="ml-2 text-xs bg-gray-100 text-gray-700 px-2 py-0.5 rounded-full">
+                  {tab.count}
+                </span>
+              )}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Connections;
